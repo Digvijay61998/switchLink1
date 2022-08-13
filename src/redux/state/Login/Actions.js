bffe4a3d61a83ed2427dfa18aee7936482c6dc11
Digvijay61998 <digvijaykadam61998@gmail.com>
2022-08-13 20:22:41 +0530

    added new changes

--- src/redux/state/Login/Actions.js
+++ src/redux/state/Login/Actions.js
@@ -21,6 +21,20 @@ export const signUserAccount = payloadData => ({
 });
 
 
+// board
+export const createBoardProcess = payloadData => ({
+    type: ActionTypes.CREATE_BOARD_PROCESS,
+    payload: payloadData,
+  });
+export const createBoardSuccess = payloadData => ({
+    type: ActionTypes.CREATE_BOARD_SUCCESS,
+    payload: payloadData,
+});
+
+export const createBoardError = (error) => ({
+    type: ActionTypes.CREATE_BOARD_ERROR,
+    error,
+  });
 
 
 
